3cf0249e433f58b1da90312515adfd526251c577
PauIGB <cwc-project@mail.ru>
2019-09-14 00:56:56 +0300

    work on logout

--- src/actions/user.js
+++ src/actions/user.js
@@ -6,47 +6,56 @@ export const LOGOUT_USER = 'LOGOUT_USER';
 export const FETCH_USER = 'FETCH_USER';
 export const ERR_USER = 'ERR_USER';
 
+const fetchUser = () => ({ type: FETCH_USER });
 const loginSuccess = data => ({ type: LOGIN_USER, user: data });
+const loggedOut = () => ({ type: LOGOUT_USER });
 
-export const login = (input, history, handleError) => dispatch => {
-  dispatch({
-    type: FETCH_USER,
-  });
-  return axios
-    .post(`${process.env.SERVER_URL_LOCAL}/user/login`, {
-      ...input,
-    })
-    .then(({ data }) => dispatch(loginSuccess(data)))
-    .then(() => history.push('/projects'))
-    .catch(e => {
-      handleError(e);
-      dispatch({
-        type: ERR_USER,
+export function login(input, history, handleError) {
+  return dispatch => {
+    dispatch(fetchUser());
+    return axios
+      .post(`${process.env.SERVER_URL_LOCAL}/user/login`, {
+        ...input,
+      })
+      .then(({ data }) => dispatch(loginSuccess(data)))
+      .then(() => history.push('/projects'))
+      .catch(e => {
+        handleError(e);
+        dispatch({
+          type: ERR_USER,
+        });
       });
-    });
-};
-
-export const logout = history => {
-  history.push('/');
-  return {
-    type: LOGOUT_USER,
   };
-};
+}
 
-export const reg = (input, history, handleError) => dispatch => {
-  dispatch({
-    type: FETCH_USER,
-  });
-  return axios
-    .post(`${process.env.SERVER_URL_LOCAL}/user/register`, {
-      ...input,
-    })
-    .then(({ data }) => dispatch(loginSuccess(data)))
-    .then(() => history.push('/projects'))
-    .catch(err => {
-      handleError(err);
-      dispatch({
-        type: ERR_USER,
+export function reg(input, history, handleError) {
+  return dispatch => {
+    dispatch(fetchUser());
+    return axios
+      .post(`${process.env.SERVER_URL_LOCAL}/user/register`, {
+        ...input,
+      })
+      .then(({ data }) => dispatch(loginSuccess(data)))
+      .then(() => history.push('/projects'))
+      .catch(err => {
+        handleError(err);
+        dispatch({
+          type: ERR_USER,
+        });
       });
-    });
-};
+  };
+}
+
+export function logout(token, history) {
+  return axios
+    .post(`${process.env.SERVER_URL_LOCAL}/user/logout`, {
+      token,
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then(() => loggedOut())
+    .catch(e => {
+      console.error(`Failed to logout: ${e}`);
+      return loggedOut();
+    })
+    .finally(() => history.push('/'));
+}
