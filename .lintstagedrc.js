1e7980632aae5ed31b15e5e8003e1a57acc0e513
PauIGB <cwc-project@mail.ru>
2019-10-14 14:46:35 +0300

    replace prettierignore with .gitignore

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,jsx}': ['eslint --fix --color --ignore-path .gitignore', 'git add'],
   '*.{css}': ['stylelint src/**/*.css --fix', 'git add'],
-  '*.{html,css,json,md}': ['prettier --write', 'git add'],
+  '*.{html,css,json,md}': ['prettier --write --ignore-path .gitignore', 'git add'],
 };
