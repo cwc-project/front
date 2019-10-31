a0e3b0a9267e5c47e0b396121f67f3a2c22f53d0
PauIGB <cwc-project@mail.ru>
2019-11-01 00:18:38 +0300

    eliminate redux-logger on production

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,12 @@
 export * from './errors';
 
 export const server = process.env.SERVER_URL;
+// export const server = (() => {
+//   if (process.env.NODE_ENV === 'development') {
+//     console.log(process.env.NODE_ENV)
+//     return process.env.SERVER_URL_LOCAL;
+//   }
+//   return process.env.SERVER_URL;
+// })();
+
+// console.log(server)
